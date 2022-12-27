3df5d9dd88360228f257d4416c19db504381c5bf
donaldcrane <donaldagbakoba@gmail.com>
2022-12-28 00:24:20 +0100

    add test dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,4 @@ import "dotenv/config";
 import router from "./routes";
 import { server } from "./core";
 
-server(process.env.LMS_PORT ?? 3000, router, process.env.LMS_BASEURL);
+server(process.env.PORT ?? 3000, router);
--- src/utils/types/interface.ts
+++ src/utils/types/interface.ts
@@ -15,32 +15,32 @@ export interface CustomRequest {
 }
 
 export interface ISeller {
-  seller_id: string
-  seller_zip_code_prefix: string
-  seller_city: string
-  seller_state: string
+  seller_id: string;
+  seller_zip_code_prefix: string;
+  seller_city: string;
+  seller_state: string;
 }
 
 export interface IOrder {
-  order_id: string
-        order_item_id: string
-        product_id: string
-        seller_id: string
-        shipping_limit_date: string
-        price:string
-        freight_value: string
+  order_id: string;
+  order_item_id: string;
+  product_id: string;
+  seller_id: string;
+  shipping_limit_date: string;
+  price: string;
+  freight_value: string;
 }
 
 export interface IProduct {
-  product_id: string
-        product_category_name: string
-        product_name_lenght: string
-        product_description_lenght: string
-        product_photos_qty: string
-        product_weight_g: string
-        product_length_cm: string
-        product_height_cm: string
-        product_width_cm: string
+  product_id: string;
+  product_category_name: string;
+  product_name_lenght: string;
+  product_description_lenght: string;
+  product_photos_qty: string;
+  product_weight_g: string;
+  product_length_cm: string;
+  product_height_cm: string;
+  product_width_cm: string;
 }
 
 export interface filterItems {
